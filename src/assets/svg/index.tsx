72ad0867127eb7a0d18739c3495da445fc184104
LuisDev8913 <luiscespedes8913@gmail.com>
2022-03-18 19:15:32 +0200

    ⚛️ assets + svg icons added

---
+++ src/assets/svg/index.tsx
@@ -0,0 +1,34 @@
+import HeartIcon from "./heart.svg";
+import ShieldIcon from "./shield.svg";
+import ArrowLinkIcon from "./arrow-link.svg";
+import StarIcon from "./star.svg";
+import TridentIcon from "./trident.svg";
+import ArrowIcon from "./arrow.svg";
+import WalletIcon from "./wallet.svg";
+
+// Socials
+import DiscordIcon from "./discord.svg";
+import FacebookIcon from "./facebook.svg";
+import InstagramIcon from "./instagram.svg";
+import MediumIcon from "./medium.svg";
+import TelegramIcon from "./telegram.svg";
+import TwitterIcon from "./twitter.svg";
+
+export const socialIcons: { [x: string]: JSX.Element } = {
+  discord: <DiscordIcon />,
+  facebook: <FacebookIcon />,
+  instagram: <InstagramIcon />,
+  medium: <MediumIcon />,
+  telegram: <TelegramIcon />,
+  twitter: <TwitterIcon />,
+};
+
+export {
+  HeartIcon,
+  ShieldIcon,
+  ArrowLinkIcon,
+  StarIcon,
+  TridentIcon,
+  ArrowIcon,
+  WalletIcon,
+};
