84eb2f23a858bbefecfda8eb8699ccd6ad926718
LuisDev8913 <luiscespedes8913@gmail.com>
2022-03-18 19:12:51 +0200

    🧹 cleanup

---
+++ src/pages/_app.tsx
@@ -0,0 +1,9 @@
+import type { AppProps } from "next/app";
+
+import "@/styles/index.css";
+
+const App = ({ Component, pageProps }: AppProps) => (
+  <Component {...pageProps} />
+);
+
+export default App;
