bc62fbd2eeba64ab7325d0e5af42f69dacb14c42
LuisDev8913 <luiscespedes8913@gmail.com>
2022-03-23 01:03:57 +0200

    ⚛️ members + question types

--- src/types.ts
+++ src/types.ts
@@ -2,3 +2,15 @@ export interface LinkType {
   url: string;
   label: string;
 }
+
+export interface Member {
+  avatar: string; // url
+  fullName: string;
+  title: string;
+  links: LinkType[];
+}
+
+export interface Question {
+  question: string;
+  answer: string | JSX.Element;
+}
