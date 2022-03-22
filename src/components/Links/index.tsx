84216fc81f493cdb4420ff3203bbc71ef6206ffb
LuisDev8913 <luiscespedes8913@gmail.com>
2022-03-23 01:03:36 +0200

    🐞 links fixes

--- src/components/Links/index.tsx
+++ src/components/Links/index.tsx
@@ -3,15 +3,18 @@ import { socialIcons } from "@/assets/svg";
 
 type Props = {
   links: LinkType[];
+  className?: string;
 };
 
-export const Links: React.FC<Props> = ({ links }) => (
+export const Links: React.FC<Props> = ({ links, className }) => (
   <>
     {links.map((link, i) => (
       <a
         href={link.url}
         key={i}
-        className='fill-[#EDEDED] hover:fill-[#F8D47A] transition ease-out delay-200'
+        className={`fill-[#EDEDED] hover:fill-[#F8D47A] hover:opacity-100 transition ease-out delay-200 ${
+          className || ""
+        }`}
         target='_blank'
         rel='noopener noreferrer nofollow'
       >
