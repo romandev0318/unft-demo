89600d32b6868f63f7dce19bdb7aca797099b447
LuisDev8913 <luiscespedes8913@gmail.com>
2022-03-23 01:00:26 +0200

    💅 hero section styling

--- src/components/Mint/index.tsx
+++ src/components/Mint/index.tsx
@@ -28,32 +28,41 @@ export const Mint: React.FC = () => {
   }, [dayX, remaining]);
 
   return (
-    <div>
-      <div>
-        <div>
-          <span>{remaining.days}</span>
-          <span>Days</span>
+    <div className='p-8 mt-16 space-y-6 backdrop-blur-md bg-[#020202]/30 rounded-[19px]'>
+      <div className='flex space-x-4'>
+        <div className='flex flex-col rounded-xl w-24 py-4 bg-[#020202]/40 border border-[#9D9B95]'>
+          <span className='text-2xl font-semibold text-[#F2F2F2]'>
+            {remaining.days}
+          </span>
+          <span className='text-sm font-bold text-[#BCBCBC]'>Days</span>
         </div>
-        <div>
-          <span>{remaining.hours}</span>
-          <span>Hours</span>
+        <div className='flex flex-col rounded-xl w-24 py-4 bg-[#020202]/40 border border-[#9D9B95]'>
+          <span className='text-2xl font-semibold text-[#F2F2F2]'>
+            {remaining.hours}
+          </span>
+          <span className='text-sm font-bold text-[#BCBCBC]'>Hours</span>
         </div>
-        <div>
-          <span>{remaining.minutes}</span>
-          <span>Minutes</span>
+        <div className='flex flex-col rounded-xl w-24 py-4 bg-[#020202]/40 border border-[#9D9B95]'>
+          <span className='text-2xl font-semibold text-[#F2F2F2]'>
+            {remaining.minutes}
+          </span>
+          <span className='text-sm font-bold text-[#BCBCBC]'>Minutes</span>
         </div>
-        <div>
-          <span>{remaining.seconds}</span>
-          <span>Seconds</span>
+        <div className='flex flex-col rounded-xl w-24 py-4 bg-[#020202]/40 border border-[#9D9B95]'>
+          <span className='text-2xl font-semibold text-[#F2F2F2]'>
+            {remaining.seconds}
+          </span>
+          <span className='text-sm font-bold text-[#BCBCBC]'>Seconds</span>
         </div>
       </div>
 
       <button
         type='button'
         disabled={dayX - Date.now() > 1}
-        className='text-[#F2CF37] hover:text-black hover:fill-black fill-[#F2CF37]'
+        className='btn-mint'
       >
-        <WalletIcon /> Mint
+        <WalletIcon className='mr-2 z-10' />
+        <span className='z-10'>Mint</span>
       </button>
     </div>
   );
--- src/sections/Hero/index.tsx
+++ src/sections/Hero/index.tsx
@@ -1,19 +1,22 @@
 import { Mint } from "@/components/Mint";
 
 export const Hero: React.FC = () => (
-  <section className='pt-40'>
-    <h1>
-      <span>Ukrainian NFT Collection</span>
+  <section className='flex flex-col w-full items-center text-center 2xl:max-w-screen-2xl mx-auto px-4 pt-[16.5rem] pb-4 mb-40'>
+    <h1 className='text-[5rem] font-extrabold leading-[6.25rem] text-[#F8D47A]'>
+      <span className='text-[#3383E0]'>Ukrainian NFT Collection</span>
       <br />
       of heroes this war
     </h1>
-    <p>Anyone can support ukraine in this extremely difficult time.</p>
+
+    <p className=' text-[1.5rem] font-medium mt-6'>
+      Anyone can support ukraine in this extremely difficult time.
+    </p>
 
     <Mint />
 
-    <p>
-      100% funds will be transferred to support the Ukrainian militaryand the
-      victims of this war.
+    <p className='text-[#F8D47A] font-semibold mt-64'>
+      100% funds will be transferred to support <br /> the Ukrainian militaryand
+      the victims of this war.
     </p>
   </section>
 );
