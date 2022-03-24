9da4078a6d75385960d4751c9549c9961e9644f9
LuisDev8913 <luiscespedes8913@gmail.com>
2022-03-24 20:27:53 +0200

    ⚙️ palyfill for smooth scrolling added

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,15 @@
 import type { AppProps } from "next/app";
+import { polyfill } from "smoothscroll-polyfill";
 
 import "@/styles/index.css";
+import { useEffect } from "react";
 
-const App = ({ Component, pageProps }: AppProps) => (
-  <Component {...pageProps} />
-);
+const App = ({ Component, pageProps }: AppProps) => {
+  useEffect(() => {
+    polyfill();
+  }, []);
+
+  return <Component {...pageProps} />;
+};
 
 export default App;
