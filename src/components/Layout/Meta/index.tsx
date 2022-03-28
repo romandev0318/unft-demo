5e57544ec5676baa0f2aba7626fd5e13bb71840a
LuisDev8913 <luiscespedes8913@gmail.com>
2022-03-28 19:11:10 +0300

    🐞 semantic fix

--- src/components/Layout/Meta/index.tsx
+++ src/components/Layout/Meta/index.tsx
@@ -2,6 +2,9 @@ import Head from "next/head";
 
 export const Meta: React.FC = () => (
   <Head>
+    <meta property='og:type' content='website' />
+    <meta property='og:url' content='https://ua-nft.io' />
+    {/* <meta property='og:image' content='/img/unft-thumbnail.webp' /> */}
     <meta
       property='og:title'
       content='Ukrainian NFT Collection of the heroes of the war'
@@ -10,9 +13,24 @@ export const Meta: React.FC = () => (
       property='og:description'
       content='Anyone can support Ukraine at this extremely difficult time'
     />
-    {/* <meta property='og:image' content='/img/unft-thumbnail.webp' /> */}
-    <meta property='og:url' content='https://ua-nft.io' />
     <meta property='og:site_name' content='UA NFT' />
+    <meta property='twitter:card' content='summary' />
+    <meta property='twitter:site' content='@UA__NFT' />
+    {/* <meta property='twitter:image' content='/img/unft-thumbnail.webp' /> */}
+    <meta
+      property='twitter:title'
+      content='Ukrainian NFT Collection of the heroes of the war'
+    />
+    <meta
+      property='twitter:description'
+      content='Anyone can support Ukraine at this extremely difficult time'
+    />
+    <meta
+      name='description'
+      content='Anyone can support Ukraine at this extremely difficult time'
+    />
+    <meta name='keywords' content='support ukraine' />
+
     <link
       rel='apple-touch-icon'
       sizes='120x120'
@@ -36,15 +54,10 @@ export const Meta: React.FC = () => (
       href='/favicon/safari-pinned-tab.svg'
       color='#f8d47a'
     />
-    <meta name='keywords' content='support ukraine' />
     <link rel='shortcut icon' href='/favicon/favicon.ico' />
-    <meta name='apple-mobile-web-app-title' content='Triplex' />
+    <meta name='apple-mobile-web-app-title' content='UA NFT' />
     <meta name='application-name' content='UA NFT' />
     <meta name='msapplication-TileColor' content='#f8d47a' />
     <meta name='theme-color' content='#f8d47a'></meta>
-    <meta
-      name='description'
-      content='Anyone can support Ukraine at this extremely difficult time'
-    />
   </Head>
 );
