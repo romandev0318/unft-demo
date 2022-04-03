33fb412890a13d501256354f557ed1b7885eecd1
LuisDev8913 <luiscespedes8913@gmail.com>
2022-04-03 21:09:47 +0300

    🖼 team section comlete

--- src/assets/team/index.tsx
+++ src/assets/team/index.tsx
@@ -1,7 +1,29 @@
-import MemberElonaImage from "@/public/img/team/elona.png";
+import AnnKharchenkoImage from "@/public/img/team/AnnKharchenko.png";
+import ArtemGolubImage from "@/public/img/team/ArtemGolub.jpg";
+import AlexsandraOtmahovaImage from "@/public/img/team/AlexsandraOtmahova.jpg";
+import DanielChermanImage from "@/public/img/team/DanielCherman.png";
+import ArtemTrubiloImage from "@/public/img/team/ArtemTrubilo.png";
+import TatianaBiryukovaImage from "@/public/img/team/TatianaBiryukova.png";
+import ViktoriiaAbramovychImage from "@/public/img/team/ViktoriiaAbramovych.jpg";
+import JohnMevisImage from "@/public/img/team/JohnMevis.jpg";
+import IhorLutzImage from "@/public/img/team/IhorLutz.png";
+import ErinFelisImage from "@/public/img/team/ErinFelis.jpeg";
+import ArtyrBershImage from "@/public/img/team/ArtyrBersh.png";
+import OleksandrShkuropatImage from "@/public/img/team/OleksandrShkuropat.png";
 
 const TeamImages = {
-  MemberElonaImage,
+  AnnKharchenkoImage,
+  ArtemGolubImage,
+  AlexsandraOtmahovaImage,
+  DanielChermanImage,
+  ArtemTrubiloImage,
+  TatianaBiryukovaImage,
+  ViktoriiaAbramovychImage,
+  JohnMevisImage,
+  IhorLutzImage,
+  ErinFelisImage,
+  ArtyrBershImage,
+  OleksandrShkuropatImage,
 };
 
 export default TeamImages;
--- src/data/team.tsx
+++ src/data/team.tsx
@@ -6,48 +6,8 @@ export const team: Member[] = [
   {
     avatar: (
       <Image
-        src={TeamImages.MemberElonaImage}
-        alt='member'
-        objectFit='cover'
-        className='rounded-3xl'
-        layout='fill'
-        placeholder='blur'
-      />
-    ),
-    fullName: "Elona Musk",
-    title: "UI / UX Designer",
-    links: [
-      {
-        url: "https://instagram.com/",
-        label: "instagram",
-      },
-    ],
-  },
-  {
-    avatar: (
-      <Image
-        src={TeamImages.MemberElonaImage}
-        alt='member'
-        objectFit='cover'
-        className='rounded-3xl'
-        layout='fill'
-        placeholder='blur'
-      />
-    ),
-    fullName: "Nadiia Kravchenko",
-    title: "Team manager",
-    links: [
-      {
-        url: "https://instagram.com/",
-        label: "instagram",
-      },
-    ],
-  },
-  {
-    avatar: (
-      <Image
-        src={TeamImages.MemberElonaImage}
-        alt='member'
+        src={TeamImages.ErinFelisImage}
+        alt='ErinFelis'
         objectFit='cover'
         className='rounded-3xl'
         layout='fill'
@@ -66,16 +26,16 @@ export const team: Member[] = [
   {
     avatar: (
       <Image
-        src={TeamImages.MemberElonaImage}
-        alt='member'
+        src={TeamImages.AlexsandraOtmahovaImage}
+        alt='AlexsandraOtmahova'
         objectFit='cover'
         className='rounded-3xl'
         layout='fill'
         placeholder='blur'
       />
     ),
-    fullName: "Victoria Abramovich",
-    title: "Staking Advisor & Director Of Implementation",
+    fullName: "Alexsandra Otmahova",
+    title: "Artist",
     links: [
       {
         url: "https://instagram.com/",
@@ -86,16 +46,16 @@ export const team: Member[] = [
   {
     avatar: (
       <Image
-        src={TeamImages.MemberElonaImage}
-        alt='member'
+        src={TeamImages.TatianaBiryukovaImage}
+        alt='TatianaBiryukova'
         objectFit='cover'
         className='rounded-3xl'
         layout='fill'
         placeholder='blur'
       />
     ),
-    fullName: "Ehor Kadurov",
-    title: "Senior Advisor",
+    fullName: "Tatiana Biryukova",
+    title: "Artist",
     links: [
       {
         url: "https://instagram.com/",
@@ -106,16 +66,16 @@ export const team: Member[] = [
   {
     avatar: (
       <Image
-        src={TeamImages.MemberElonaImage}
-        alt='member'
+        src={TeamImages.AnnKharchenkoImage}
+        alt='AnnKharchenko'
         objectFit='cover'
         className='rounded-3xl'
         layout='fill'
         placeholder='blur'
       />
     ),
-    fullName: "Tatyana Rubka",
-    title: "Ui / Ux Designer",
+    fullName: "Ann Kharchenko",
+    title: "PR Agent",
     links: [
       {
         url: "https://instagram.com/",
@@ -126,16 +86,16 @@ export const team: Member[] = [
   {
     avatar: (
       <Image
-        src={TeamImages.MemberElonaImage}
-        alt='member'
+        src={TeamImages.ViktoriiaAbramovychImage}
+        alt='Viktoriia Abramovych'
         objectFit='cover'
         className='rounded-3xl'
         layout='fill'
         placeholder='blur'
       />
     ),
-    fullName: "Dmitrii Kolbasa",
-    title: "Staking Advisor & Director Of Implementation",
+    fullName: "Viktoriia Abramovych",
+    title: "Artist consultant, SMM",
     links: [
       {
         url: "https://instagram.com/",
@@ -146,55 +106,15 @@ export const team: Member[] = [
   {
     avatar: (
       <Image
-        src={TeamImages.MemberElonaImage}
-        alt='member'
+        src={TeamImages.JohnMevisImage}
+        alt='JohnMevis'
         objectFit='cover'
         className='rounded-3xl'
         layout='fill'
         placeholder='blur'
       />
     ),
-    fullName: "Serhei Lomachenko",
-    title: "Senior Advisor",
-    links: [
-      {
-        url: "https://instagram.com/",
-        label: "instagram",
-      },
-    ],
-  },
-  {
-    avatar: (
-      <Image
-        src={TeamImages.MemberElonaImage}
-        alt='member'
-        objectFit='cover'
-        className='rounded-3xl'
-        layout='fill'
-        placeholder='blur'
-      />
-    ),
-    fullName: "Dmitrii Hevrev",
-    title: "NFT Specialist",
-    links: [
-      {
-        url: "https://instagram.com/",
-        label: "instagram",
-      },
-    ],
-  },
-  {
-    avatar: (
-      <Image
-        src={TeamImages.MemberElonaImage}
-        alt='member'
-        objectFit='cover'
-        className='rounded-3xl'
-        layout='fill'
-        placeholder='blur'
-      />
-    ),
-    fullName: "Elona Musk",
+    fullName: "John Mevis",
     title: "UI / UX Designer",
     links: [
       {
@@ -206,16 +126,116 @@ export const team: Member[] = [
   {
     avatar: (
       <Image
-        src={TeamImages.MemberElonaImage}
-        alt='member'
+        src={TeamImages.IhorLutzImage}
+        alt='IhorLutz'
         objectFit='cover'
         className='rounded-3xl'
         layout='fill'
         placeholder='blur'
       />
     ),
-    fullName: "Elona Musk",
-    title: "UI / UX Designer",
+    fullName: "Ihor Lutz",
+    title: "Lead graphic designer",
+    links: [
+      {
+        url: "https://instagram.com/",
+        label: "instagram",
+      },
+    ],
+  },
+  {
+    avatar: (
+      <Image
+        src={TeamImages.OleksandrShkuropatImage}
+        alt='OleksandrShkuropat'
+        objectFit='cover'
+        className='rounded-3xl'
+        layout='fill'
+        placeholder='blur'
+      />
+    ),
+    fullName: "Oleksandr Shkuropat",
+    title: "Marketing manager",
+    links: [
+      {
+        url: "https://instagram.com/",
+        label: "instagram",
+      },
+    ],
+  },
+  {
+    avatar: (
+      <Image
+        src={TeamImages.DanielChermanImage}
+        alt='DanielCherman'
+        objectFit='cover'
+        className='rounded-3xl'
+        layout='fill'
+        placeholder='blur'
+      />
+    ),
+    fullName: "Daniel Cherman",
+    title: "Head of Development & Integration. Co-founder CGT group",
+    links: [
+      {
+        url: "https://instagram.com/",
+        label: "instagram",
+      },
+    ],
+  },
+  {
+    avatar: (
+      <Image
+        src={TeamImages.ArtemTrubiloImage}
+        alt='ArtemTrubilo'
+        objectFit='cover'
+        className='rounded-3xl'
+        layout='fill'
+        placeholder='blur'
+      />
+    ),
+    fullName: "Artem Trubilo",
+    title: "Co-founder CGT group",
+    links: [
+      {
+        url: "https://instagram.com/",
+        label: "instagram",
+      },
+    ],
+  },
+  {
+    avatar: (
+      <Image
+        src={TeamImages.ArtemGolubImage}
+        alt='ArtemGolub'
+        objectFit='cover'
+        className='rounded-3xl'
+        layout='fill'
+        placeholder='blur'
+      />
+    ),
+    fullName: "Artem Golub",
+    title: "Co-founder",
+    links: [
+      {
+        url: "https://instagram.com/",
+        label: "instagram",
+      },
+    ],
+  },
+  {
+    avatar: (
+      <Image
+        src={TeamImages.ArtyrBershImage}
+        alt='ArtyrBersh'
+        objectFit='cover'
+        className='rounded-3xl'
+        layout='fill'
+        placeholder='blur'
+      />
+    ),
+    fullName: "Artyr Bersh",
+    title: "Co-founder",
     links: [
       {
         url: "https://instagram.com/",
